0fc553643e11c9330e2fb54bcaff82e7e304968e
CodyPellom <paulpellom@gmail.com>
2022-05-30 09:36:24 -0400

    circle animation

--- javascript/heroAnimation.js
+++ javascript/heroAnimation.js
@@ -1,38 +1,62 @@
-var circle = document.getElementById('hero-circle');
-var animatedSection = document.getElementById('animated-hero-section');
-var cNode = circle;
-var cloneCircle = cNode.cloneNode(true);
-animatedSection.appendChild(cloneCircle);
+let runBtn = document.getElementById('runBtn');
+let circle = document.getElementById('hero-circle');
+let circle2 = document.getElementById('hero-circle2');
+let circle3 = document.getElementById('hero-circle3');
+let circle4 = document.getElementById('hero-circle4');
+let circle5 = document.getElementById('hero-circle5');
+let circle6 = document.getElementById('hero-circle6');
+let circle7 = document.getElementById('hero-circle7');
+let circle8 = document.getElementById('hero-circle8');
+let circle9 = document.getElementById('hero-circle9');
+let circle10 = document.getElementById('hero-circle10');
+let circle11 = document.getElementById('hero-circle11');
+let circle12 = document.getElementById('hero-circle12');
+let animatedSection = document.getElementById('animated-hero-section');
+
+let colorList = ["13c561", "3d3733", "2510b5", "51f08f", "fb391e", "b2dc35", "fc7101", "3b7a07"];
+
+setInterval(function(){
+    initHero(circle);
+    initHero(circle2);
+    initHero(circle3);
+    initHero(circle4);
+    initHero(circle5);
+    initHero(circle6);
+    initHero(circle7);
+    initHero(circle8);
+    initHero(circle9);
+    initHero(circle10);
+    initHero(circle11);
+    initHero(circle12);
+}, 2000)
 
 function initHero(target) {
     anime({
         targets: target,
-        translateX: randomNumber100,
-        translateY: randomNumber100
+        backgroundColor: randColor,
+        width: randomNumber(150),
+        height: randomNumber(150),
+        translateX: randomNumber(800),
+        translateY: randomNumber(50),
+        easing: 'easeInOutSine',
+        duration: 1500
     });
 }
 
-initHero(cNode);
-
-function randomNumber1000() {
-    var random_int = Math.floor(Math.random() * 1000);
-    return random_int;
-}
-
-function randomNumber100() {
-    var random_int = Math.floor(Math.random() * 100);
-    return random_int;
-}
-
-function randomNumber10() {
-    var random_int = Math.floor(Math.random() * 10);
+function randomNumber(length) {
+    let random_int = Math.floor(Math.random() * length);
     return random_int;
 }
 
 function genCircle() {
-var cDiv = document.createElement('div');
+let cDiv = document.createElement('div');
 cDiv.className = 'gen-circle';
 cDiv.style.width = `${randomNumber100}`+ px;
 cDiv.style.height = `${randomNumber100}` + px;
 return cDiv;
+}
+
+function randColor(){
+    let random_int = Math.floor(Math.random() * 8);
+    return `#${colorList[random_int]}`;
 }
